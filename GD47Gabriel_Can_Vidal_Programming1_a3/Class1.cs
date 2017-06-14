4598282947efced618ea5f120e89b51580a8ca23
gd47gabriel <gd47gabriel@GD122>
2017-06-13 19:07:36 -0700

    Add project files.

---
+++ GD47Gabriel_Can_Vidal_Programming1_a3/Class1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GD47Gabriel_Can_Vidal_Programming1_a3
+{
+    class petfight
+    {
+        private int m_Attack;
+        private int m_Defense;
+        private int m_HitPoints;
+        private string m_petName;
+
+        public int Attack
+        {
+            get
+            {
+                return m_Attack;
+            }
+
+            set
+            {
+                m_Attack = value;
+            }
+        }
+
+        public int Defense
+        {
+            get
+            {
+                return m_Defense;
+            }
+
+            set
+            {
+                m_Defense = value;
+            }
+        }
+
+        public int HitPoints
+        {
+            get
+            {
+                return m_HitPoints;
+            }
+
+            set
+            {
+                m_HitPoints = value;
+            }
+        }
+
+            // constructor
+
+            public petfight(string Gary)
+        {
+            m_HitPoints = 100;
+            m_Attack = 50;
+            m_Defense = 30;
+            m_petName = Gary;
+        }
+            
+
+        
+
+
+
+
+
+
+    }
+    }
+}
---
+++ GD47Gabriel_Can_Vidal_Programming1_a3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GD47Gabriel_Can_Vidal_Programming1_a3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
